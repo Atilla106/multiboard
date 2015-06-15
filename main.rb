cef6502e97ce805d9d7fb45e193a0a40f1e3fd2b
Thomas Papillon <tp@irrealis.me>
2015-06-15 22:47:38 +0200

    move CSS into its own file

--- main.rb
+++ main.rb
@@ -3,6 +3,8 @@ require 'sinatra'
 require './lib/train'
 require './lib/weather'
 
+set :public_folder, File.dirname(__FILE__) + '/content'
+
 get '/' do
   @time = Time.now
   @trains = Train.from_sncf[0..11]
