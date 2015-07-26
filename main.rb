37ed57a81e18d41fa0f295f86b29d7cd324b2189
Flavien Raynaud <flavien.raynaud@gmail.com>
2015-07-26 12:58:39 +0200

    use config.ru

--- main.rb
+++ main.rb
@@ -1,13 +1,18 @@
-require 'sinatra'
+require 'sinatra/base'
+require 'tilt/haml'
 
 require './lib/train'
 require './lib/weather'
 
-set :public_folder, File.dirname(__FILE__) + '/content'
+class Main < Sinatra::Base
+  set :public_folder, File.dirname(__FILE__) + '/content'
 
-get '/' do
-  @time = Time.now
-  @trains = Train.from_sncf[0..11]
-  @weather = Weather.new
-  haml :index
+  get '/' do
+    @time = Time.now
+    @trains = Train.from_sncf[0..11]
+    @weather = Weather.new
+    haml :index
+  end
+
+  run! if __FILE__ == $0
 end
